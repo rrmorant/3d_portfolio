b829867a975c3d6425a15286914f428100c6e72a
Ron M <duecel@icloud.com>
2024-08-12 21:58:49 -0700

    finished Navbar for now, moving to the Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,8 +20,9 @@ const Navbar = () => {
 						window.scrollTo(0, 0);
 					}}>
 					<img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-					<p className="text-white text-[18px] font-bold cursor-pointer">
-						Ron <span className="sm:block hidden"> | 3D Portfolio</span>
+					<p className="text-white text-[18px] font-bold cursor-pointer flex">
+						Ron &nbsp;
+						<span className="sm:block hidden"> | 3D Portfolio</span>
 					</p>
 				</Link>
 				<ul className="list-none hidden sm:flex flex-row gap-10">
@@ -47,7 +48,23 @@ const Navbar = () => {
 					<div
 						className={`${
 							!toggle ? "hidden" : "flex"
-						} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}></div>
+						} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+						<ul className="list-none flex justify-end items-start flex-col gap-4">
+							{navLinks.map((link) => (
+								<li
+									key={link.id}
+									className={`${
+										active === link.title ? "text-white" : "text-secondary"
+									}font-poppins font-medium cursor-pointer text-[16px]`}
+									onClick={() => {
+										setToggle(!toggle);
+										setActive(link.title);
+									}}>
+									<a href={`#${link.id}`}>{link.title}</a>
+								</li>
+							))}
+						</ul>
+					</div>
 				</div>
 			</div>
 		</nav>
